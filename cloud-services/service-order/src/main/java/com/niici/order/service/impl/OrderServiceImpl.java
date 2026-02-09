06c0d8be2e6ed552d996519de34242bb9fd2ebc7
niici <nichao@hzxjyc.com>
2026-02-09 13:46:12 +0800

    csr证书示例

--- cloud-services/service-order/src/main/java/com/niici/order/service/impl/OrderServiceImpl.java
+++ cloud-services/service-order/src/main/java/com/niici/order/service/impl/OrderServiceImpl.java
@@ -38,7 +38,51 @@ public class OrderServiceImpl implements OrderService {
         log.info("timeOut: {}, autoConfirm: {}", timeOut, autoConfirm);
     }
 
+
+    public static void main(String[] args) {
+        String aaa =
+                """
+                {
+                    "name": "niici"
+                }
+                """;
+        System.out.println(aaa);
+
+        String mode = "单机模式"    ;
+        String result = switch (mode) {
+            case "单机模式" -> "standAlone";
+            default -> {
+               yield "unknown";
+            }
+        };
+    }
+
     /*private Product getProductRemote(Long productId) {
 
     }*/
+
+
+    /*CSR证书签名请求文件生成命令：openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.csr
+
+    # 国家代码 - CN
+    Country Name (2 letter code) [AU]:CN
+    # 省份 - Zhejiang
+    State or Province Name (full name) [Some-State]: Zhejiang
+    # 城市名称（地级市 / 直辖市）- Hangzhou
+    Locality Name (eg, city) []: Hangzhou
+    # 组织 / 公司名称（企业 / 机构全称）
+    Organization Name (eg, company) [Internet Widgits Pty Ltd]: 稠银金租
+    # 部门名称（公司内部的部门，如技术部、财务部）- 技术部：IT Department、财务部：Finance Department
+    Organizational Unit Name (eg, section) []: IT Department
+    # 申请 HTTPS 证书：填域名（FQDN）、申请代码签名证书：填开发者 / 公司名称；
+    Common Name (e.g. server FQDN or YOUR name) []: www.example.com
+    # 联系邮箱, CA 机构会通过此邮箱发送证书、通知审核结果
+    Email Address []: yourname@example.com
+
+    Please enter the following 'extra' attributes
+    to be sent with your certificate request
+    # CSR 的挑战密码，用于后续撤销证书、重新申请时的身份验证
+    A challenge password []:123456
+    # 可选的公司名称, 是对前面Organization Name的补充，几乎所有 CA 都忽略此字段
+    An optional company name []:123456*/
 }
