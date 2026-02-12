4a3337dae0004b051de414cdd6e4db998ba860d0
niici <nichao@hzxjyc.com>
2026-02-12 10:11:14 +0800

    feignClient自定义调用重试策略

--- cloud-services/service-order/src/main/java/com/niici/order/config/FeignClientConfiguration.java
+++ cloud-services/service-order/src/main/java/com/niici/order/config/FeignClientConfiguration.java
@@ -1,8 +1,11 @@
 package com.niici.order.config;
 
 import feign.Logger;
+import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * openFeign 日志配置类
@@ -13,5 +16,11 @@ public class FeignClientConfiguration {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    Retryer feignRetryer() {
+        // 自定义feignClient重试策略, 参数一次为初始间隔时间、最大间隔时间、最大重试次数
+        return new Retryer.Default(200, TimeUnit.SECONDS.toMillis(1), 3);
     }
 }
