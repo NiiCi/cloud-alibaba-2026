e07fffda9f9b0d1bbcdd7cf6c24a43918c0efd7b
niici <nichao@hzxjyc.com>
2026-03-16 10:54:28 +0800

    seata: mysql、redis、nacos、seata - docker部署文件上传

--- docker/seata/seata-server-init.sql
+++ docker/seata/seata-server-init.sql
@@ -4,53 +4,70 @@ USE seata;
 
 -- 全局事务表
 CREATE TABLE IF NOT EXISTS `global_table` (
-                                              `xid` VARCHAR(128) NOT NULL,
-    `transaction_id` BIGINT NULL DEFAULT NULL,
-    `status` TINYINT NOT NULL,
-    `application_id` VARCHAR(32) NULL DEFAULT NULL,
-    `transaction_service_group` VARCHAR(32) NULL DEFAULT NULL,
-    `transaction_name` VARCHAR(128) NULL DEFAULT NULL,
-    `timeout` INT NULL DEFAULT NULL,
-    `begin_time` BIGINT NULL DEFAULT NULL,
-    `application_data` VARCHAR(2000) NULL DEFAULT NULL,
-    `gmt_create` DATETIME NULL DEFAULT NULL,
-    `gmt_modified` DATETIME NULL DEFAULT NULL,
+    `xid`                       VARCHAR(128) NOT NULL,
+    `transaction_id`            BIGINT,
+    `status`                    TINYINT      NOT NULL,
+    `application_id`            VARCHAR(32),
+    `transaction_service_group` VARCHAR(32),
+    `transaction_name`          VARCHAR(128),
+    `timeout`                   INT,
+    `begin_time`                BIGINT,
+    `application_data`          VARCHAR(2000),
+    `gmt_create`                DATETIME,
+    `gmt_modified`              DATETIME,
     PRIMARY KEY (`xid`),
-    INDEX `idx_status_gmt_modified` (`status`, `gmt_modified`),
-    INDEX `idx_transaction_id` (`transaction_id`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    KEY `idx_status_gmt_modified` (`status`, `gmt_modified`),
+    KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 -- 分支事务表
 CREATE TABLE IF NOT EXISTS `branch_table` (
-                                              `branch_id` BIGINT NOT NULL,
-                                              `xid` VARCHAR(128) NOT NULL,
-    `transaction_id` BIGINT NULL DEFAULT NULL,
-    `resource_group_id` VARCHAR(32) NULL DEFAULT NULL,
-    `resource_id` VARCHAR(256) NULL DEFAULT NULL,
-    `branch_type` VARCHAR(8) NULL DEFAULT NULL,
-    `status` TINYINT NULL DEFAULT NULL,
-    `client_id` VARCHAR(64) NULL DEFAULT NULL,
-    `application_data` VARCHAR(2000) NULL DEFAULT NULL,
-    `gmt_create` DATETIME(6) NULL DEFAULT NULL,
-    `gmt_modified` DATETIME(6) NULL DEFAULT NULL,
+    `branch_id`         BIGINT       NOT NULL,
+    `xid`               VARCHAR(128) NOT NULL,
+    `transaction_id`    BIGINT,
+    `resource_group_id` VARCHAR(32),
+    `resource_id`       VARCHAR(256),
+    `branch_type`       VARCHAR(8),
+    `status`            TINYINT,
+    `client_id`         VARCHAR(64),
+    `application_data`  VARCHAR(2000),
+    `gmt_create`        DATETIME(6),
+    `gmt_modified`      DATETIME(6),
     PRIMARY KEY (`branch_id`),
-    INDEX `idx_xid` (`xid`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    KEY `idx_xid` (`xid`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
--- 锁表
+-- 全局锁表
 CREATE TABLE IF NOT EXISTS `lock_table` (
-                                            `row_key` VARCHAR(128) NOT NULL,
-    `xid` VARCHAR(128) NULL DEFAULT NULL,
-    `transaction_id` BIGINT NULL DEFAULT NULL,
-    `branch_id` BIGINT NOT NULL,
-    `resource_id` VARCHAR(256) NULL DEFAULT NULL,
-    `table_name` VARCHAR(32) NULL DEFAULT NULL,
-    `pk` VARCHAR(36) NULL DEFAULT NULL,
-    `status` TINYINT NOT NULL DEFAULT 0 COMMENT '0:locked,1:rollbacking',
-    `gmt_create` DATETIME NULL DEFAULT NULL,
-    `gmt_modified` DATETIME NULL DEFAULT NULL,
+    `row_key`        VARCHAR(128) NOT NULL,
+    `xid`            VARCHAR(128),
+    `transaction_id` BIGINT,
+    `branch_id`      BIGINT       NOT NULL,
+    `resource_id`    VARCHAR(256),
+    `table_name`     VARCHAR(32),
+    `pk`             VARCHAR(36),
+    `status`         TINYINT      NOT NULL DEFAULT 0 COMMENT '0:locked ,1:rollbacking',
+    `gmt_create`     DATETIME,
+    `gmt_modified`   DATETIME,
     PRIMARY KEY (`row_key`),
-    INDEX `idx_status` (`status`),
-    INDEX `idx_branch_id` (`branch_id`),
-    INDEX `idx_xid` (`xid`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    KEY `idx_status` (`status`),
+    KEY `idx_branch_id` (`branch_id`),
+    KEY `idx_xid` (`xid`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- 分布式锁表（2.x 新增）
+CREATE TABLE IF NOT EXISTS `distributed_lock` (
+    `lock_key`   CHAR(20)  NOT NULL,
+    `lock_value` VARCHAR(20) NOT NULL,
+    `expire`     BIGINT,
+    PRIMARY KEY (`lock_key`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire)
+VALUES ('AsyncCommitting', ' ', 0);
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire)
+VALUES ('RetryCommitting', ' ', 0);
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire)
+VALUES ('RetryRollbacking', ' ', 0);
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire)
+VALUES ('TxTimeoutCheck', ' ', 0);
